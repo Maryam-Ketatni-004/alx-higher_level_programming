b56e2ae1b3d4b76e6cd942d8bc6ab2aa85234a9b
Maryam-Ketatni-004 <maryamketatni00.com>
2024-04-29 05:45:13 -0700

    pp

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-try {
-  const data = fs.readFileSync(file, 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(err);
+
+process.argv[0] = 'node';
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+if (process.argv.length === 3) {
+  const filePath = process.argv[2];
+  readFile(filePath);
 }
